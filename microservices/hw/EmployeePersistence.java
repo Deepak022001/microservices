45d4b82b568f43b108e71feb1abad960692db223
Deepak singh <studydipu@gmail.com>
2025-02-26 14:59:03 +0530

    Delete microservices/hw directory

--- microservices/hw/EmployeePersistence.java
+++
@@ -1,7 +0,0 @@
-
-public class EmployeePersistence {
-    public void saveToDatabase(Employee employee) {
-        System.out.println("Saving employee " + employee.getName() + " to database...");
-
-    }
-}
