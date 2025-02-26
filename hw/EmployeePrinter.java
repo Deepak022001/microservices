b5f18526314efbe5d5e5d84fdd970d1decdfafb9
Deepak singh <studydipu@gmail.com>
2025-02-26 14:53:30 +0530

    Delete hw directory

--- hw/EmployeePrinter.java
+++
@@ -1,7 +0,0 @@
-
-public class EmployeePrinter {
-    public void printEmployeeDetails(Employee employee) {
-        System.out.println("Employee Name: " + employee.getName());
-        System.out.println("Salary: " + employee.getSalary());
-    }
-}
